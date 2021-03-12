60849966c97c7dc4bd1e29bea5e9768695effc11
martinfoldyna <martin.foldyna@me.com>
2021-03-12 10:22:45 +0100

    💄 Added AntDesign

--- src/modules/application/components/HomePage/HomePage.jsx
+++ src/modules/application/components/HomePage/HomePage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Button } from "antd";
 
-const HomePage = ({ styles }) => <>HomePage</>;
+const HomePage = ({ styles }) => (
+  <div className={styles.container}>
+    <h1>HomePage</h1>
+    <Button type='primary'>Primary</Button>
+  </div>
+);
 
 HomePage.propTypes = {
   styles: PropTypes.shape({
