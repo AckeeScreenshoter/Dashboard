296a4c2b1ee0516926160345ab743356150f4f3f
Martin Foldyna <martin.foldyna@ackee.cz>
2021-05-31 19:13:04 +0200

    :lipstick: Add all fields into DataCard

--- src/modules/ui/components/DataCard/DataCard.jsx
+++ src/modules/ui/components/DataCard/DataCard.jsx
@@ -1,25 +1,54 @@
 import React from "react";
-
-import * as felaRules from "./DataCard.styles";
-import useFelaEnhanced from "hooks/useFelaEnhanced";
 import { Card, Typography } from "antd";
 import { FormattedMessage } from "react-intl";
+import dayjs from "dayjs";
+import { AndroidFilled, AppleFilled, CopyOutlined } from "@ant-design/icons";
+
+import videoImg from "assets/images/thumbnail_vid.png";
+import useFelaEnhanced from "hooks/useFelaEnhanced";
+
+import * as felaRules from "./DataCard.styles";
 import Button from "../Button";
 
 const DataCard = ({ message }) => {
   const { styles } = useFelaEnhanced(felaRules);
 
   return (
-    <Card hoverable>
-      <img src={message.image} alt={message.appName} />
+    <Card
+      hoverable
+      cover={
+        <img
+          src={message.mediaUploaded ? message.image : videoImg}
+          alt={message.appName}
+        />
+      }
+      className={styles.card}
+    >
       <div className={styles.contentWrapper}>
-        <Typography.Title level={3}>{message.appName}</Typography.Title>
+        <div className={styles.cardHeader}>
+          <div>
+            <Typography.Text className={styles.createdAt}>
+              {dayjs(message.date.seconds * 1000).format("DD. MM. YYYY, HH:mm")}
+            </Typography.Text>
+            <Typography.Title level={3}>{message.appName}</Typography.Title>
+          </div>
+          {message.deviceMake === "samsung" ? (
+            <AndroidFilled />
+          ) : (
+            <AppleFilled />
+          )}
+        </div>
         {message.note && (
           <Typography.Paragraph>{message.note}</Typography.Paragraph>
         )}
-        <Button type="secondary">
-          <FormattedMessage id="card.button.delete" />
-        </Button>
+        <div>
+          <Button type="secondary">
+            <FormattedMessage id="card.button.delete" />
+          </Button>
+          <Button type="primary" icon={<CopyOutlined />}>
+            <FormattedMessage id="card.button.copy" />
+          </Button>
+        </div>
       </div>
     </Card>
   );
