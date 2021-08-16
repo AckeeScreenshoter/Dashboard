cd32d32c63714a9aeb74c725fde0e925ba385c45
Grixi <43199888+Grixi@users.noreply.github.com>
2021-08-16 14:41:11 +0200

    💄 select form

--- src/modules/ui/components/FilterBar/FilterBar.styles.js
+++ src/modules/ui/components/FilterBar/FilterBar.styles.js
@@ -9,6 +9,12 @@ export const select = ({ theme }) => ({
         borderRadius: theme.borderRadiuses.small + '!important',
         borderColor: theme.colors.secondaryText,
     },
+    ':hover': {
+        borderColor: theme.colors.secondaryText,
+    },
+    ':focus': {
+        borderColor: theme.colors.secondaryText,
+    },
 });
 export const formItem = ({ theme }) => ({
     '& .ant-form-item-label': {
@@ -18,5 +24,17 @@ export const formItem = ({ theme }) => ({
         fontSize: '80%',
         color: theme.colors.secondaryText,
         fontWeight: '600',
+    },
+});
+export const dropdown = ({ theme }) => ({
+    borderRadius: theme.borderRadiuses.small,
+    '& .ant-select-item-selected': {
+        backgroundColor: theme.colors.primaryBorderHover,
+    },
+    '& .ant-select-item-option-active': {
+        backgroundColor: theme.colors.primaryBorderHover,
+    },
+    '& .ant-select-item-option-selected:not(.ant-select-item-option-disabled)': {
+        backgroundColor: theme.colors.primaryBorderHover,
     },
 });
