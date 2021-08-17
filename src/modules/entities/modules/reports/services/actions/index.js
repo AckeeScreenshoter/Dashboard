4e7a38c77086d7ff26a15ba09e452005ccb2e664
Grixi <43199888+Grixi@users.noreply.github.com>
2021-08-17 14:43:41 +0200

    ✨ delete screenshot

--- src/modules/entities/modules/reports/services/actions/index.js
+++ src/modules/entities/modules/reports/services/actions/index.js
@@ -1,2 +1,6 @@
 export { default as fetchReportsTypes } from './types';
+export { default as deleteReportType } from './types';
+
 export { default as fetchReports } from './fetchReports';
+export { default as deleteReport } from './deleteReport';
+x;
---
+++ src/modules/entities/modules/reports/services/sagas/deleteReport.js
@@ -0,0 +1,28 @@
+import { put, call } from 'redux-saga/effects';
+import { takeLatestRequest } from '@ackee/antonio-utils';
+
+import { firestore } from 'config/firebase';
+import * as log from 'config/loglevel';
+import { createUIErrorMessage } from '../../../../utils/errors';
+import { deleteReport as actions, deleteReportType } from '../actions';
+
+function* deleteReport() {
+    try {
+        yield call(actions.fetchReportsSuccess());
+    } catch (error) {
+        log.error(error);
+
+        const uiError = createUIErrorMessage(error);
+        yield put(actions.fetchReportsFailure(uiError));
+    }
+}
+
+export default function* () {
+    yield takeLatestRequest(
+        {
+            DELETE: deleteReportType.DELETE_REPORT_REQUEST,
+            cancelTask: actions.deleteReportCancel,
+        },
+        deleteReport,
+    );
+}
