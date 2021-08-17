b4c372aa3e7fcfcc3d4c8b0c7e0352f059ee38a2
Grixi <43199888+Grixi@users.noreply.github.com>
2021-08-17 09:24:20 +0200

    🎨 add props form firebase

--- src/modules/ui/components/CardDetail/CardDetail.jsx
+++ src/modules/ui/components/CardDetail/CardDetail.jsx
@@ -12,7 +12,19 @@ import NoteForm from '../NoteForm';
 const CardDetail = ({ message, onCancel, visible }) => {
     const { styles } = useFelaEnhanced(felaRules);
     const { Text, Paragraph } = Typography;
-    const { appName, platform, appVersion, date, note, deviceModel, osVersion, bundleId } = message;
+    const {
+        appName,
+        platform,
+        appVersion,
+        date,
+        note,
+        bundleId,
+        deviceModel,
+        deviceMake,
+        osVersion,
+        mediaUploaded,
+        buildNumber,
+    } = message;
 
     return (
         <div id="modalMount" className={styles.cardDetail}>
@@ -85,15 +97,15 @@ const CardDetail = ({ message, onCancel, visible }) => {
                         <Text className={styles.subTitle}>
                             <FormattedMessage id="card.detail.mediaUploaded" />
                         </Text>
-                        <Paragraph className={styles.subName}>mediaUploaded</Paragraph>
+                        <Paragraph className={styles.subName}>{mediaUploaded?.toString()}</Paragraph>
                         <Text className={styles.subTitle}>
                             <FormattedMessage id="card.detail.deviceMake" />
                         </Text>
-                        <Paragraph className={styles.subName}>deviceMake</Paragraph>
+                        <Paragraph className={styles.subName}>{deviceMake}</Paragraph>
                         <Text className={styles.subTitle}>
                             <FormattedMessage id="card.detail.bundleId" />
                         </Text>
-                        <Paragraph className={styles.subName}>bundleId</Paragraph>
+                        <Paragraph className={styles.subName}>{bundleId}</Paragraph>
                         <Text className={styles.subTitle}>
                             <FormattedMessage id="card.detail.customData" />
                         </Text>
@@ -101,7 +113,7 @@ const CardDetail = ({ message, onCancel, visible }) => {
                         <Text className={styles.subTitle}>
                             <FormattedMessage id="card.detail.buildNumber" />
                         </Text>
-                        <Paragraph className={styles.subName}>buildNumber</Paragraph>
+                        <Paragraph className={styles.subName}>{buildNumber}</Paragraph>
                     </Col>
                     <Col sm={12} className={styles.screenBox}>
                         <img alt={'screenshot'} className={styles.screenshot} src={'screenshot.png'} />
