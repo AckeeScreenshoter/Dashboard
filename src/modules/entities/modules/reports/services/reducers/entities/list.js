1517f03ed5a3836563dfe9fd2756a2e005957f9b
Grixi <43199888+Grixi@users.noreply.github.com>
2021-08-24 16:11:02 +0200

    ♻️  polish creating of const

--- src/modules/entities/modules/reports/services/reducers/entities/list.js
+++ src/modules/entities/modules/reports/services/reducers/entities/list.js
@@ -3,13 +3,14 @@ import { types } from '../../actions';
 const initialState = [];
 
 export default function listReportsReducer(state = initialState, action) {
-    const { params, meta } = action;
     switch (action.type) {
         case types.FETCH_REPORTS_SUCCESS:
             return [...action.payload];
         case types.ADD_NOTE_SUCCESS:
+            const { params } = action;
             return [...state.map(item => (item.id === params.id ? { ...item, note: params.note } : item))];
         case types.DELETE_REPORT_SUCCESS:
+            const { meta } = action;
             return [...state.filter(item => item.id !== meta.id)];
         default:
             return state;
