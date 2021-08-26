a2296164b4fc8e6b40fbedcb2d56a964bb6d33b1
Grixi <43199888+Grixi@users.noreply.github.com>
2021-08-26 13:19:57 +0200

    🔒️ add todo

--- src/config/config.development.js
+++ src/config/config.development.js
@@ -6,6 +6,7 @@ const config = {
     api: {
         base: `https://api-${appName}-development.ack.ee/api`,
     },
+
     appUrl: isEnvDevelopment ? 'http://localhost:3000' : `https://api-${appName}-development.ack.ee`,
     devTools: true,
 };
--- src/config/config.production.js
+++ src/config/config.production.js
@@ -4,6 +4,8 @@ const config = {
     api: {
         base: 'https://skeleton',
     },
+    // TODO: add appUrl
+    // appUrl: isEnvProduction ? 'http://localhost:3000' : `https://api-${appName}-production.ack.ee`,
 };
 
 export default config;
