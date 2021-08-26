bb3f405a58554bb593a1dd9eacee0375c14516f7
Grixi <43199888+Grixi@users.noreply.github.com>
2021-08-26 13:10:49 +0200

    edited config variable

--- src/config/config.development.js
+++ src/config/config.development.js
@@ -1,10 +1,12 @@
+import { isEnvDevelopment } from '../constants';
+
 const appName = process.env.REACT_APP_NAME;
 
 const config = {
     api: {
         base: `https://api-${appName}-development.ack.ee/api`,
     },
-    appUrl: 'http://localhost:3000',
+    appUrl: isEnvDevelopment ? 'http://localhost:3000' : `https://api-${appName}-development.ack.ee`,
     devTools: true,
 };
 
