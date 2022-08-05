ab45dae66b2b83640cd875bea9a27966dbb9a9e3
Jiří Zdvomka <jiri.zdvomka@ackee.cz>
2022-08-05 14:20:37 +0200

    🐛 Fix app base URL

--- src/config/config.development.js
+++ src/config/config.development.js
@@ -1,13 +1,9 @@
-import { isEnvDevelopment } from '../constants';
-
 const appName = process.env.REACT_APP_NAME;
 
 const config = {
     api: {
         base: `https://api-${appName}-development.ack.ee/api`,
     },
-
-    appUrl: isEnvDevelopment ? 'http://localhost:3000' : `https://api-${appName}-development.ack.ee`,
     devTools: true,
 };
 
